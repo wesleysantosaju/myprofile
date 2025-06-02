154ad6bec133bf9f974d299990bd4ab84c761d71
wesleysantosdev <wvsantos15@gmail.com>
2025-06-02 13:46:36 -0300

    up site

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Wesley - Back-End Specialist</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <div class="card">
+    <img src="img/logo.jpeg" alt="Foto de Wesley Santos" class="profile-photo">
+    <h1>Wesley Santos</h1>
+    <p class="subtitle">Back-End Specialist</p>
+    <p class="stack">Java | Oracle DB | Spring</p>
+    <p class="expertise">Expert em APIs RESTful</p>
+
+    <div class="social-buttons">
+      <a href="https://www.linkedin.com/in/wesley-santos-a3a11ba5/" target="_blank" class="btn linkedin">LinkedIn</a>
+      <a href="https://instagram.com/odevjava" target="_blank" class="btn instagram">Instagram</a>
+      <a href="https://github.com/wesleysantosaju" target="_blank" class="btn github">GitHub</a>
+    </div>
+  </div>
+</body>
+</html>
